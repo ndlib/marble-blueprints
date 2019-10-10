e08ad40d5f0a8ca148821dcb59138b960c9fa1b2
Justin Gondron <jgondron@nd.edu>
2019-10-10 16:42:17 -0400

    Create deployment for nulib/iiif-serverless
    This adds a deployment pipeline that will deploy the app using the provided template.yml in the app repo. This also adds a new CloudFormation that will create a CustomDomain for the API so that our hostnames and certs will match. Creating this in cdk would have made an odd mix between the two. In the future, we'll want to refactor to use a cdk based stack for the application as well, so I'm assuming it will be easier to rewrite this part at that time.
    
    I'm putting this cdk code at the root of the deploy/cdk directory. Personally, I think this should be a multistack cdk application so that on deploy you can just choose which components (stacks) you want to deploy. In addition, doing it as a multistack application will let cdk take care of a lot of the cross-stack property sharing for us (although it will be via export/import only atm). Unfortunately, this will look a little confusing for a bit since we have some applications within subdirectories for the other components (elasticsearch and web-kiosk).

---
+++ deploy/cdk/lib/iiif-serverless/deployment-pipeline.ts
@@ -0,0 +1,316 @@
+import { CloudFormationCapabilities } from '@aws-cdk/aws-cloudformation';
+import { BuildSpec, PipelineProject, LinuxBuildImage } from '@aws-cdk/aws-codebuild';
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+import codepipelineActions = require('@aws-cdk/aws-codepipeline-actions');
+import { PolicyStatement } from '@aws-cdk/aws-iam';
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
+import ssm = require('@aws-cdk/aws-ssm');
+import cdk = require('@aws-cdk/core');
+import { Runtime } from '@aws-cdk/aws-lambda';
+import { Fn } from '@aws-cdk/core';
+import { NamespacedPolicy, GlobalActions } from '../namespaced-policy';
+import { Topic } from '@aws-cdk/aws-sns';
+import { ManualApprovalAction } from '@aws-cdk/aws-codepipeline-actions';
+
+export interface IDeploymentPipelineStackProps extends cdk.StackProps {
+  readonly oauthTokenPath: string;
+  readonly appRepoOwner: string;
+  readonly appRepoName: string;
+  readonly appSourceBranch: string;
+  readonly infraRepoOwner: string;
+  readonly infraRepoName: string;
+  readonly infraSourceBranch: string;
+  readonly qaRepoOwner: string;
+  readonly qaRepoName: string;
+  readonly qaSourceBranch: string;
+  readonly imageSourceBucketPath: string;
+  readonly stackPrefix: string;
+  readonly domainStackName: string;
+  readonly hostnamePrefix: string;
+};
+
+export class DeploymentPipelineStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props: IDeploymentPipelineStackProps) {
+    super(scope, id, props);
+
+    const appRepoUrl = `https://github.com/${props.appRepoOwner}/${props.appRepoName}`;
+    const resolvedDomain = Fn.importValue(`${props.domainStackName}:DomainName`);
+    const hostedZone = Fn.importValue(`${props.domainStackName}:Zone`);
+    const testHost = `${props.hostnamePrefix}-test.${resolvedDomain}`;
+    const testStackName = `${props.stackPrefix}-test`;
+    const testCDNStackName = `${props.stackPrefix}-cdn-test`;
+    const prodHost = `${props.hostnamePrefix}.${resolvedDomain}`;
+    const prodStackName = `${props.stackPrefix}-prod`;
+    const prodCDNStackName = `${props.stackPrefix}-cdn-prod`;
+    const sourceBucketParam = ssm.StringParameter.fromStringParameterName(this, 'SourceBucket', props.imageSourceBucketPath);
+
+    const artifactBucket = new Bucket(this, 'artifactBucket', { 
+      encryption: BucketEncryption.KMS_MANAGED, 
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Source Actions
+    const appSourceArtifact = new codepipeline.Artifact('AppCode');
+    const appSourceAction = new codepipelineActions.GitHubSourceAction({
+        actionName: 'AppCode',
+        branch: props.appSourceBranch,
+        oauthToken: cdk.SecretValue.secretsManager(props.oauthTokenPath, { jsonField: 'oauth' }),
+        output: appSourceArtifact,
+        owner: props.appRepoOwner,
+        repo: props.appRepoName,
+    });
+    const qaSourceArtifact = new codepipeline.Artifact('QACode');
+    const qaSourceAction = new codepipelineActions.GitHubSourceAction({
+        actionName: 'QACode',
+        branch: props.qaSourceBranch,
+        oauthToken: cdk.SecretValue.secretsManager(props.oauthTokenPath, { jsonField: 'oauth' }),
+        output: qaSourceArtifact,
+        owner: props.qaRepoOwner,
+        repo: props.qaRepoName,
+    });
+    const infraSourceArtifact = new codepipeline.Artifact('InfraCode');
+    const infraSourceAction = new codepipelineActions.GitHubSourceAction({
+        actionName: 'InfraCode',
+        branch: props.infraSourceBranch,
+        oauthToken: cdk.SecretValue.secretsManager(props.oauthTokenPath, { jsonField: 'oauth' }),
+        output: infraSourceArtifact,
+        owner: props.infraRepoOwner,
+        repo: props.infraRepoName,
+    });
+
+    const builtCodeArtifact = new codepipeline.Artifact('BuiltCode');
+    const build = new PipelineProject(this, 'IIIFServerlessBuild', {
+      buildSpec: BuildSpec.fromObject({
+        phases: {
+          install: {
+            commands: [
+              'cd $CODEBUILD_SRC_DIR/dependencies/nodejs',
+              'npm install',
+            ],
+            'runtime-versions': Runtime.NODEJS_10_X,
+          },
+          build: {
+            commands: [
+              'cd $CODEBUILD_SRC_DIR',
+              `aws cloudformation package \
+                --template-file ./template.yml \
+                --s3-bucket ${artifactBucket.bucketName} \
+                --s3-prefix 'CloudformationPackages' \
+                --output-template-file package_output.yml`,
+            ]
+          },
+        },
+        version: '0.2',
+        artifacts: { 
+          files: ['package_output.yml']
+        }
+      }),
+    });
+    build.addToRolePolicy(new PolicyStatement({
+      actions: [
+        's3:ListBucket',
+        's3:GetObject',
+        's3:PutObject',
+      ],
+      resources: [artifactBucket.bucketArn]
+    }));
+    const buildAction = new codepipelineActions.CodeBuildAction({
+      actionName: 'Build',
+      input: appSourceArtifact, 
+      outputs: [builtCodeArtifact],
+      project: build,
+    });
+
+    const builtTemplatePath = new codepipeline.ArtifactPath(builtCodeArtifact, 'package_output.yml');
+    const deployTestAction = new codepipelineActions.CloudFormationCreateUpdateStackAction({
+      actionName: 'DeployAPI',
+      templatePath: builtTemplatePath,
+      stackName: testStackName,
+      adminPermissions: false,
+      parameterOverrides: {
+        SourceBucket: sourceBucketParam.stringValue
+      },
+      capabilities: [
+        CloudFormationCapabilities.AUTO_EXPAND,
+        CloudFormationCapabilities.ANONYMOUS_IAM,
+      ],
+      runOrder: 1,
+    });
+    const cdnTemplatePath = new codepipeline.ArtifactPath(infraSourceArtifact, 'deploy/cloudformation/iiif-serverless-cdn.yml');
+    const deployTestCDNAction = new codepipelineActions.CloudFormationCreateUpdateStackAction({
+      actionName: 'DeployCDN',
+      templatePath: cdnTemplatePath,
+      stackName: testCDNStackName,
+      adminPermissions: false,
+      parameterOverrides: {
+        HostnamePrefix: `${props.hostnamePrefix}-test`,
+        DomainStackName: props.domainStackName,
+        APIStackName: testStackName,
+      },
+      capabilities: [
+        CloudFormationCapabilities.AUTO_EXPAND,
+        CloudFormationCapabilities.ANONYMOUS_IAM,
+      ],
+      runOrder: 2,
+    });
+
+    const smokeTestsProject = new PipelineProject(this, 'ResearchAwardSmokeTests', {
+      buildSpec: BuildSpec.fromObject({
+        phases: {
+          build: {
+            commands: [
+              `echo '{
+                "values": [
+                  {"key": "image-server-host","value": "${testHost}"}
+                ]
+              }' > test_env.json`,
+              `newman run newman/smoke.json -e test_env.json`,
+            ],
+          },
+        },
+        version: '0.2',
+      }),
+      environment: {
+        buildImage: LinuxBuildImage.fromDockerRegistry('postman/newman'),
+      },
+    });
+    const smokeTestsAction = new codepipelineActions.CodeBuildAction({
+      input: qaSourceArtifact, 
+      project: smokeTestsProject,
+      actionName: 'SmokeTests',
+      runOrder: 98,
+    });
+
+    // Approval
+    const approvalTopic = new Topic(this, 'ApprovalTopic');
+    const approvalAction = new ManualApprovalAction({
+      actionName: 'Approval',
+      additionalInformation: `A new version of ${appRepoUrl} has been deployed to https://${testHost} and is awaiting your approval. If you approve these changes, they will be deployed to https://${prodHost}.`,
+      notificationTopic: approvalTopic,
+      runOrder: 99, // This should always be the last action in the stage
+    });
+
+    const deployProdAction = new codepipelineActions.CloudFormationCreateUpdateStackAction({
+      actionName: 'DeployAPI',
+      templatePath: builtTemplatePath,
+      stackName: prodStackName,
+      adminPermissions: false,
+      parameterOverrides: {
+        SourceBucket: sourceBucketParam.stringValue
+      },
+      capabilities: [
+        CloudFormationCapabilities.AUTO_EXPAND,
+        CloudFormationCapabilities.ANONYMOUS_IAM,
+      ],
+      runOrder: 1,
+    });
+    const deployProdCDNAction = new codepipelineActions.CloudFormationCreateUpdateStackAction({
+      actionName: 'DeployCDN',
+      templatePath: cdnTemplatePath,
+      stackName: prodCDNStackName,
+      adminPermissions: false,
+      parameterOverrides: {
+        HostnamePrefix: props.hostnamePrefix,
+        DomainStackName: props.domainStackName,
+        APIStackName: prodStackName,
+      },
+      capabilities: [
+        CloudFormationCapabilities.AUTO_EXPAND,
+        CloudFormationCapabilities.ANONYMOUS_IAM,
+      ],
+      runOrder: 2,
+    });
+
+    const smokeTestsProdProject = new PipelineProject(this, 'ResearchAwardSmokeTestsProd', {
+      buildSpec: BuildSpec.fromObject({
+        phases: {
+          build: {
+            commands: [
+              `echo '{
+                "values": [
+                  {"key": "image-server-host","value": "${prodHost}"}
+                ]
+              }' > test_env.json`,
+              `newman run newman/smoke.json -e test_env.json`,
+            ],
+          },
+        },
+        version: '0.2',
+      }),
+      environment: {
+        buildImage: LinuxBuildImage.fromDockerRegistry('postman/newman'),
+      },
+    });
+    const smokeTestsProdAction = new codepipelineActions.CodeBuildAction({
+      input: qaSourceArtifact, 
+      project: smokeTestsProdProject,
+      actionName: 'SmokeTests',
+      runOrder: 98,
+    });
+
+    // Pipeline
+    const pipeline = new codepipeline.Pipeline(this, 'DeploymentPipeline', {
+      artifactBucket,
+      stages: [
+        {
+          actions: [appSourceAction, qaSourceAction, infraSourceAction],
+          stageName: 'Source',
+        },
+        {
+          actions: [buildAction],
+          stageName: 'Build',
+        },
+        {
+          actions: [deployTestAction, deployTestCDNAction, smokeTestsAction, approvalAction],
+          stageName: 'Test',
+        },
+        {
+          actions: [deployProdAction, deployProdCDNAction, smokeTestsProdAction],
+          stageName: 'Production',
+        }
+      ],
+    });
+
+    deployTestAction.addToDeploymentRolePolicy(new PolicyStatement({
+      actions: [
+        's3:ListBucket',
+        's3:GetObject',
+      ],
+      resources: [
+        artifactBucket.bucketArn,
+        `${artifactBucket.bucketArn}/*`
+      ]
+    }));
+
+    deployTestAction.addToDeploymentRolePolicy(NamespacedPolicy.transform());
+    deployTestAction.addToDeploymentRolePolicy(NamespacedPolicy.iamRole(testStackName));
+    deployTestAction.addToDeploymentRolePolicy(NamespacedPolicy.lambda(testStackName));
+    deployTestAction.addToDeploymentRolePolicy(NamespacedPolicy.api());
+
+    deployTestCDNAction.addToDeploymentRolePolicy(NamespacedPolicy.ssm(testCDNStackName));
+    deployTestCDNAction.addToDeploymentRolePolicy(NamespacedPolicy.route53RecordSet(hostedZone));
+    deployTestCDNAction.addToDeploymentRolePolicy(NamespacedPolicy.apiDomain(testHost));
+    deployTestCDNAction.addToDeploymentRolePolicy(NamespacedPolicy.globals([GlobalActions.Cloudfront, GlobalActions.Route53]));
+
+    deployProdAction.addToDeploymentRolePolicy(new PolicyStatement({
+      actions: [
+        's3:ListBucket',
+        's3:GetObject',
+      ],
+      resources: [
+        artifactBucket.bucketArn,
+        `${artifactBucket.bucketArn}/*`
+      ]
+    }));
+
+    deployProdAction.addToDeploymentRolePolicy(NamespacedPolicy.transform());
+    deployProdAction.addToDeploymentRolePolicy(NamespacedPolicy.iamRole(prodStackName));
+    deployProdAction.addToDeploymentRolePolicy(NamespacedPolicy.lambda(prodStackName));
+    deployProdAction.addToDeploymentRolePolicy(NamespacedPolicy.api());
+
+    deployProdCDNAction.addToDeploymentRolePolicy(NamespacedPolicy.ssm(prodCDNStackName));
+    deployProdCDNAction.addToDeploymentRolePolicy(NamespacedPolicy.route53RecordSet(hostedZone));
+    deployProdCDNAction.addToDeploymentRolePolicy(NamespacedPolicy.apiDomain(prodHost));
+    deployProdCDNAction.addToDeploymentRolePolicy(NamespacedPolicy.globals([GlobalActions.Cloudfront, GlobalActions.Route53]));
+  }
+}

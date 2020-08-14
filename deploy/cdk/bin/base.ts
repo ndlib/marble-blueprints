f83980372d6b8ff7fa794bd1888953c5e1ccf1b9
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-08-14 13:01:09 -0400

    Add ESLint.

--- deploy/cdk/bin/base.ts
+++ deploy/cdk/bin/base.ts
@@ -1,61 +1,61 @@
 #!/usr/bin/env node
-import { App, ConstructNode } from '@aws-cdk/core';
-import { StackTags } from '@ndlib/ndlib-cdk';
-import 'source-map-support/register';
-import { FoundationStack } from '../lib/foundation';
-import IIIF = require('../lib/iiif-serverless');
-import userContent = require('../lib/user-content');
-import imageProcessing = require('../lib/image-processing');
+import { App, ConstructNode } from '@aws-cdk/core'
+import { StackTags } from '@ndlib/ndlib-cdk'
+import 'source-map-support/register'
+import { FoundationStack } from '../lib/foundation'
+import IIIF = require('../lib/iiif-serverless')
+import userContent = require('../lib/user-content')
+import imageProcessing = require('../lib/image-processing')
 
-const allContext = JSON.parse(process.env.CDK_CONTEXT_JSON ?? "{}");
+const allContext = JSON.parse(process.env.CDK_CONTEXT_JSON ?? "{}")
 
-const app = new App();
+const app = new App()
 
 // Globs all kvp from context of the form "namespace:key": "value"
 // and flattens it to an object of the form "key": "value"
-const getContextByNamespace = (ns: string) : any => {
-  const result: any = {};
-  const prefix = `${ns}:`;
+const getContextByNamespace = (ns: string): any => {
+  const result: any = {}
+  const prefix = `${ns}:`
   for (const [key, value] of Object.entries(allContext)) {
     if(key.startsWith(prefix)){
-      const flattenedKey =  key.substr(prefix.length);
-      result[flattenedKey] = value;
+      const flattenedKey =  key.substr(prefix.length)
+      result[flattenedKey] = value
     }
-  };
-  return result;
+  }
+  return result
 }
 
 const getRequiredContext = (key: string) => {
-  const value = app.node.tryGetContext(key);
+  const value = app.node.tryGetContext(key)
   if(value === undefined || value === null)
-    throw new Error(`Context key '${key}' is required.`);
-  return value;
+    throw new Error(`Context key '${key}' is required.`)
+  return value
 }
 
 // Get context keys that are required by all stacks
-const owner = getRequiredContext('owner');
-const contact = getRequiredContext('contact');
-const namespace = getRequiredContext('namespace');
-const envName = getRequiredContext('env');
-const contextEnv = getRequiredContext('environments')[envName];
+const owner = getRequiredContext('owner')
+const contact = getRequiredContext('contact')
+const namespace = getRequiredContext('namespace')
+const envName = getRequiredContext('env')
+const contextEnv = getRequiredContext('environments')[envName]
 if(contextEnv === undefined || contextEnv === null)
-  throw new Error(`Context key 'environments.${envName}' is required.`);
+  throw new Error(`Context key 'environments.${envName}' is required.`)
 
 // The environment objects defined in our context are a mixture of properties.
 // Need to decompose these into a cdk env object and other required stack props
-const env = { account: contextEnv.account, region: contextEnv.region, name: envName };
-const { useVpcId, domainName, createDns, useExistingDnsZone, slackNotifyStackName } = contextEnv;
+const env = { account: contextEnv.account, region: contextEnv.region, name: envName }
+const { useVpcId, domainName, createDns, useExistingDnsZone, slackNotifyStackName } = contextEnv
 
-const oauthTokenPath = app.node.tryGetContext('oauthTokenPath');
+const oauthTokenPath = app.node.tryGetContext('oauthTokenPath')
 
 const foundationStack = new FoundationStack(app, `${namespace}-foundation`, {
   env,
   domainName,
   useExistingDnsZone,
   useVpcId,
-});
+})
 
-const imageServiceContext = getRequiredContext('iiifImageService');
+const imageServiceContext = getRequiredContext('iiifImageService')
 new IIIF.DeploymentPipelineStack(app, `${namespace}-image-service-deployment`, {
   env,
   createDns,
@@ -63,10 +63,10 @@ new IIIF.DeploymentPipelineStack(app, `${namespace}-image-service-deployment`, {
   oauthTokenPath,
   namespace,
   foundationStack,
-  ...imageServiceContext
-});
+  ...imageServiceContext,
+})
 
-const userContentContext = getContextByNamespace('userContent');
+const userContentContext = getContextByNamespace('userContent')
 const userContentProps = {
   env,
   foundationStack,
@@ -74,7 +74,7 @@ const userContentProps = {
   namespace,
   ...userContentContext,
 }
-new userContent.UserContentStack(app, `${namespace}-user-content`, userContentProps);
+new userContent.UserContentStack(app, `${namespace}-user-content`, userContentProps)
 new userContent.DeploymentPipelineStack(app, `${namespace}-user-content-deployment`, {
   contextEnvName: envName,
   oauthTokenPath,
@@ -82,15 +82,15 @@ new userContent.DeploymentPipelineStack(app, `${namespace}-user-content-deployme
   contact,
   slackNotifyStackName,
   ...userContentProps,
-});
+})
 
-const imageProcessingContext = getContextByNamespace('imageProcessing');
+const imageProcessingContext = getContextByNamespace('imageProcessing')
 const imageProcessingProps = {
   env,
   foundationStack,
   ...imageProcessingContext,
-};
-new imageProcessing.ImagesStack(app, `${namespace}-image-processing`, imageProcessingProps);
+}
+new imageProcessing.ImagesStack(app, `${namespace}-image-processing`, imageProcessingProps)
 new imageProcessing.DeploymentPipelineStack(app, `${namespace}-image-processing-deployment`, {
   contextEnvName: envName,
   oauthTokenPath,
@@ -98,5 +98,5 @@ new imageProcessing.DeploymentPipelineStack(app, `${namespace}-image-processing-
   contact,
   namespace,
   ...imageProcessingProps,
-});
-app.node.applyAspect(new StackTags());
+})
+app.node.applyAspect(new StackTags())
--- deploy/cdk/test/foundation/stack.test.ts
+++ deploy/cdk/test/foundation/stack.test.ts
@@ -1,154 +1,154 @@
-import { expect as expectCDK, haveResource, haveResourceLike, MatchStyle, matchTemplate, anything } from '@aws-cdk/assert';
-import cdk = require('@aws-cdk/core');
-import { FoundationStack } from '../../lib/foundation';
-import helpers = require('../helpers');
-import { Vpc } from '@aws-cdk/aws-ec2';
+import { expect as expectCDK, haveResource, haveResourceLike, MatchStyle, matchTemplate, anything } from '@aws-cdk/assert'
+import cdk = require('@aws-cdk/core')
+import { FoundationStack } from '../../lib/foundation'
+import helpers = require('../helpers')
+import { Vpc } from '@aws-cdk/aws-ec2'
 
 describe('FoundationStack', () => {
   describe('VPC', () => {
     describe('when not given an existing VPC', () => {
       const stack = () => {
-        const app = new cdk.App();
+        const app = new cdk.App()
         return new FoundationStack(app, 'MyTestStack', {
           domainName: 'test.edu',
-        });
-      };
+        })
+      }
 
       test('creates a VPC', () => {
-        const subject = stack();
-        expectCDK(subject).to(haveResource('AWS::EC2::VPC'));
-      });
+        const subject = stack()
+        expectCDK(subject).to(haveResource('AWS::EC2::VPC'))
+      })
 
       test('creates an internet gateway', () => {
-        const subject = stack();
-        expectCDK(subject).to(haveResource('AWS::EC2::InternetGateway'));
+        const subject = stack()
+        expectCDK(subject).to(haveResource('AWS::EC2::InternetGateway'))
       })
 
       test('creates two private subnets and two public subnets', () => {
-        const subject = stack();
+        const subject = stack()
         expectCDK(subject).to(haveResourceLike('AWS::EC2::Subnet', {
           AvailabilityZone: {
-            "Fn::Select": [0, { "Fn::GetAZs": "" }]
+            "Fn::Select": [0, { "Fn::GetAZs": "" }],
           },
           "MapPublicIpOnLaunch": false,
-        }));
+        }))
         expectCDK(subject).to(haveResourceLike('AWS::EC2::Subnet', {
           AvailabilityZone: {
-            "Fn::Select": [1, { "Fn::GetAZs": "" }]
+            "Fn::Select": [1, { "Fn::GetAZs": "" }],
           },
           "MapPublicIpOnLaunch": false,
-        }));
+        }))
         expectCDK(subject).to(haveResourceLike('AWS::EC2::Subnet', {
           AvailabilityZone: {
-            "Fn::Select": [0, { "Fn::GetAZs": "" }]
+            "Fn::Select": [0, { "Fn::GetAZs": "" }],
           },
           "MapPublicIpOnLaunch": true,
-        }));
+        }))
         expectCDK(subject).to(haveResourceLike('AWS::EC2::Subnet', {
           AvailabilityZone: {
-            "Fn::Select": [1, { "Fn::GetAZs": "" }]
+            "Fn::Select": [1, { "Fn::GetAZs": "" }],
           },
           "MapPublicIpOnLaunch": true,
-        }));
+        }))
       })
 
       test('creates a NAT gateway for each public subnet', () => {
-        const subject = stack();
+        const subject = stack()
         expectCDK(subject).to(haveResourceLike('AWS::EC2::NatGateway', {
           "SubnetId": {
-            "Ref": "VPCPublicSubnet1SubnetB4246D30"
+            "Ref": "VPCPublicSubnet1SubnetB4246D30",
           },
-        }));
+        }))
         expectCDK(subject).to(haveResourceLike('AWS::EC2::NatGateway', {
           "SubnetId": {
-            "Ref": "VPCPublicSubnet2Subnet74179F39"
+            "Ref": "VPCPublicSubnet2Subnet74179F39",
           },
-        }));
-      });
-    });
+        }))
+      })
+    })
 
     describe('when given an existing VPC', () => {
       beforeEach(() => {
-        helpers.mockVpcFromLookup();
-      });
+        helpers.mockVpcFromLookup()
+      })
 
       const stack = () => {
-        const app = new cdk.App();
+        const app = new cdk.App()
         return new FoundationStack(app, 'MyTestStack', {
           domainName: 'test.edu',
           useVpcId: 'abc123',
-        });
-      };
+        })
+      }
 
       test('does not create a VPC', () => {
-        const subject = stack();
-        expectCDK(subject).notTo(haveResource('AWS::EC2::VPC'));
-      });
+        const subject = stack()
+        expectCDK(subject).notTo(haveResource('AWS::EC2::VPC'))
+      })
 
       test('does not create an internet gateway', () => {
-        const subject = stack();
-        expectCDK(subject).notTo(haveResource('AWS::EC2::InternetGateway'));
-      });
+        const subject = stack()
+        expectCDK(subject).notTo(haveResource('AWS::EC2::InternetGateway'))
+      })
 
       test('does not create any subnets', () => {
-        const subject = stack();
-        expectCDK(subject).notTo(haveResource('AWS::EC2::Subnet'));
-      });
+        const subject = stack()
+        expectCDK(subject).notTo(haveResource('AWS::EC2::Subnet'))
+      })
 
       test('creates a NAT gateway for each public subnet', () => {
-        const subject = stack();
-        expectCDK(subject).notTo(haveResource('AWS::EC2::NatGateway'));
-      });
-    });
-  });
+        const subject = stack()
+        expectCDK(subject).notTo(haveResource('AWS::EC2::NatGateway'))
+      })
+    })
+  })
 
   describe('Domain', () => {
     describe('when useExistingDnsZone is false', () => {
       const stack = () => {
-        const app = new cdk.App();
+        const app = new cdk.App()
         return new FoundationStack(app, 'MyTestStack', {
           domainName: 'test.edu',
           useExistingDnsZone: false,
-        });
-      };
+        })
+      }
 
       test('creates a wildcard certificate for the domain using the zone as validation', () => {
-        const subject = stack();
+        const subject = stack()
         expectCDK(subject).to(haveResourceLike('AWS::CertificateManager::Certificate', {
           DomainName: '*.test.edu',
           DomainValidationOptions: [{
             DomainName: '*.test.edu',
             HostedZoneId: {
-              Ref: "HostedZoneDB99F866"
-            }
+              Ref: "HostedZoneDB99F866",
+            },
           }],
           ValidationMethod: "DNS",
-        }));
-      });
+        }))
+      })
 
       test('creates a Route53 for the domain', () => {
-        const subject = stack();
+        const subject = stack()
         expectCDK(subject).to(haveResourceLike('AWS::Route53::HostedZone', {
           Name: 'test.edu.',
-        }));
-      });
-    });
+        }))
+      })
+    })
 
     describe('when useExistingDnsZone is true', () => {
       beforeEach(() => {
-        helpers.mockHostedZoneFromLookup();
-      });
+        helpers.mockHostedZoneFromLookup()
+      })
 
       const stack = () => {
-        const app = new cdk.App();
+        const app = new cdk.App()
         return new FoundationStack(app, 'MyTestStack', {
           domainName: 'test.edu',
           useExistingDnsZone: true,
-        });
-      };
+        })
+      }
 
       test('creates a wildcard certificate for the domain using DNS validation', () => {
-        const subject = stack();
+        const subject = stack()
         // THEN
         /* https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-certificatemanager.CertificateValidation.html
         IMPORTANT: If hostedZone is not specified, DNS records must be added manually and the stack will not complete creating until the records are added. 
@@ -156,36 +156,36 @@ describe('FoundationStack', () => {
         expectCDK(subject).to(haveResourceLike('AWS::CertificateManager::Certificate', {
           DomainName: '*.test.edu',
           ValidationMethod: "DNS",
-        }));
-      });
+        }))
+      })
 
       test('does not create a Route53 for the domain', () => {
-        const subject = stack();
-        expectCDK(subject).notTo(haveResource('AWS::Route53::HostedZone'));
-      });
+        const subject = stack()
+        expectCDK(subject).notTo(haveResource('AWS::Route53::HostedZone'))
+      })
 
-    });
-  });
+    })
+  })
  
   describe('Cluster', () => {
     test('creates a cluster', () => {
-      const app = new cdk.App();
+      const app = new cdk.App()
       // WHEN
       const stack = new FoundationStack(app, 'MyTestStack', {
         domainName: 'test.edu',
-      });
+      })
       // THEN
-      expectCDK(stack).to(haveResource('AWS::ECS::Cluster'));
-    });
-  });
+      expectCDK(stack).to(haveResource('AWS::ECS::Cluster'))
+    })
+  })
 
   describe('Logs', () => {
     test('creates a shared log bucket with ten year retention and one version for backup', () => {
-      const app = new cdk.App();
+      const app = new cdk.App()
       // WHEN
       const stack = new FoundationStack(app, 'MyTestStack', {
         domainName: 'test.edu',
-      });
+      })
       // THEN
       expectCDK(stack).to(haveResourceLike('AWS::S3::Bucket', {
         AccessControl: "LogDeliveryWrite",
@@ -194,60 +194,60 @@ describe('FoundationStack', () => {
             Status: "Enabled",
             ExpirationInDays: 3650,
             NoncurrentVersionExpirationInDays: 1,
-          }]
-        }
-      }));
-    });
+          }],
+        },
+      }))
+    })
 
     test('creates a shared log group with one year retention', () => {
-      const app = new cdk.App();
+      const app = new cdk.App()
       // WHEN
       const stack = new FoundationStack(app, 'MyTestStack', {
         domainName: 'test.edu',
-      });
+      })
       // THEN
       expectCDK(stack).to(haveResourceLike('AWS::Logs::LogGroup', {
         RetentionInDays: 365,
-      }));
-    });
-  });
+      }))
+    })
+  })
 
   describe('Public S3 Bucket', () => {
     test('creates an s3 bucket that is accessible to specific sites', () => {
-      const app = new cdk.App();
+      const app = new cdk.App()
       // WHEN
       const stack = new FoundationStack(app, 'MyTestStack', {
         domainName: 'test.edu',
-      });
+      })
       // THEN
       expectCDK(stack).to(haveResourceLike('AWS::S3::Bucket', {
         CorsConfiguration: {
           CorsRules: [
             {
               AllowedHeaders: [
-                "*"
+                "*",
               ],
               AllowedMethods: [
-                "GET"
+                "GET",
               ],
               AllowedOrigins: [
                 "*.test.edu",
               ],
-              MaxAge: 3600
-            }
-          ]
+              MaxAge: 3600,
+            },
+          ],
         },
         LoggingConfiguration: {
           DestinationBucketName: {
-            Ref: "LogBucketCC3B17E8"
+            Ref: "LogBucketCC3B17E8",
           },
-          LogFilePrefix: "s3/data-broker/"
+          LogFilePrefix: "s3/data-broker/",
         },
         WebsiteConfiguration: {
-          IndexDocument: "index.html"
-        }
-      }));
+          IndexDocument: "index.html",
+        },
+      }))
 
-    });
-  });
-});
+    })
+  })
+})

7a8e3e568fea952dfd155038107f2f8a3b114c9b
Justin Gondron <jgondron@nd.edu>
2020-09-16 12:38:29 -0400

    Add encryption

--- deploy/cdk/lib/foundation/pipeline-foundation-stack.ts
+++ deploy/cdk/lib/foundation/pipeline-foundation-stack.ts
@@ -1,5 +1,6 @@
-import { Construct, Stack, StackProps } from "@aws-cdk/core"
+import { Construct, Stack, StackProps, RemovalPolicy } from "@aws-cdk/core"
 import { ArtifactBucket } from '@ndlib/ndlib-cdk'
+import { BucketEncryption } from "@aws-cdk/aws-s3"
 
 export class PipelineFoundationStack extends Stack {
   /**
@@ -9,6 +10,8 @@ export class PipelineFoundationStack extends Stack {
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
-    this.artifactBucket = new ArtifactBucket(this, 'Bucket', {})
+    this.artifactBucket = new ArtifactBucket(this, 'Bucket', {
+      encryption: BucketEncryption.KMS_MANAGED,
+    })
   }
 }

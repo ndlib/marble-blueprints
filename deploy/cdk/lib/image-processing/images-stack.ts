d7137c5c45cbf12c3e92cf2b9fd95c4a3a417ada
Ryan Doughty <rdought1@nd.edu>
2021-03-24 14:37:41 -0400

    remove rbsc event listener

--- deploy/cdk/lib/image-processing/images-stack.ts
+++ deploy/cdk/lib/image-processing/images-stack.ts
@@ -2,20 +2,15 @@ import * as ecs from '@aws-cdk/aws-ecs'
 import { Rule, Schedule } from '@aws-cdk/aws-events'
 import { EcsTask } from '@aws-cdk/aws-events-targets'
 import * as iam from '@aws-cdk/aws-iam'
-import * as lambda from '@aws-cdk/aws-lambda'
-import * as s3 from '@aws-cdk/aws-s3'
 import cdk = require('@aws-cdk/core')
 import { Annotations } from '@aws-cdk/core'
 import fs = require('fs')
 import { FoundationStack } from '../foundation'
-import { S3NotificationToLambdaCustomResource } from './s3ToLambda'
 import { ManifestPipelineStack } from '../manifest-pipeline'
 import { MaintainMetadataStack } from '../maintain-metadata'
 
 export interface ImagesStackProps extends cdk.StackProps {
-  readonly lambdaCodePath: string;
   readonly dockerfilePath: string;
-  readonly rbscBucketName: string;
   readonly manifestPipelineStack: ManifestPipelineStack;
   readonly foundationStack: FoundationStack;
   readonly maintainMetadataStack: MaintainMetadataStack;
@@ -25,45 +20,9 @@ export class ImagesStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: ImagesStackProps) {
     super(scope, id, props)
 
-    const rbscBucketName = props.rbscBucketName
-    const rbscBucket = s3.Bucket.fromBucketName(this, 'RbscBucket', rbscBucketName)
-    const processBucket = props.manifestPipelineStack.processBucket
     const imageBucket = props.foundationStack.publicBucket
     const graphqlApiUrlKeyPath = props.maintainMetadataStack.graphqlApiUrlKeyPath
     const graphqlApiKeyKeyPath = props.maintainMetadataStack.graphqlApiKeyKeyPath
-
-    /* get rbsc bucket and attach object listener */
-    const changedImgRole = new iam.Role(this, 'S3ImageRole', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-    })
-    changedImgRole.addToPolicy(new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      resources: [
-        processBucket.bucketArn + '/*',
-      ],
-      actions: [
-        "s3:PutObject",
-      ],
-    }))
-    const awsLambdaLoggingPolicy = 'service-role/AWSLambdaBasicExecutionRole'
-    const roleLoggingPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName(awsLambdaLoggingPolicy)
-    changedImgRole.addManagedPolicy(roleLoggingPolicy)
-
-    if(!fs.existsSync(props.lambdaCodePath)) {
-      Annotations.of(this).addError(`Cannot deploy this stack. Asset path not found ${props.lambdaCodePath}`)
-      return
-    }
-    const imageTracker = new lambda.Function(this, 'Handler', {
-      runtime: lambda.Runtime.PYTHON_3_8,
-      code: lambda.Code.fromAsset(props.lambdaCodePath),
-      handler: 'generate.handler',
-      environment: {
-        PROCESS_BUCKET: processBucket.bucketName,
-      },
-      role: changedImgRole,
-    })
-    // https://github.com/aws/aws-cdk/issues/2004
-    new S3NotificationToLambdaCustomResource(this, id, rbscBucket, imageTracker)
 
     const taskRole = new iam.Role(this, 'MarbleImageTaskRole', {
       assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
@@ -71,8 +30,6 @@ export class ImagesStack extends cdk.Stack {
     taskRole.addToPolicy(new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
       resources: [
-        processBucket.bucketArn,
-        processBucket.bucketArn + '/*',
         imageBucket.bucketArn,
         imageBucket.bucketArn + '/*',
       ],
@@ -89,15 +46,6 @@ export class ImagesStack extends cdk.Stack {
         cdk.Fn.sub('arn:aws:ssm:${AWS::Region}:${AWS::AccountId}:parameter/all/marble*'),
       ],
       actions: ["ssm:Get*"],
-    }))
-    taskRole.addToPolicy(new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      resources: [
-        rbscBucket.bucketArn + '/*',
-      ],
-      actions: [
-        "s3:GetObject",
-      ],
     }))
     taskRole.addToPolicy(new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
@@ -125,9 +73,6 @@ export class ImagesStack extends cdk.Stack {
         streamPrefix: `${this.stackName}-AppContainer`,
       }),
       environment: {
-        LEVEL0: 'enable',
-        RBSC_BUCKET: rbscBucketName,
-        PROCESS_BUCKET: processBucket.bucketName,
         IMAGE_BUCKET: imageBucket.bucketName,
         GRAPHQL_API_URL_KEY_PATH: graphqlApiUrlKeyPath,
         GRAPHQL_API_KEY_KEY_PATH: graphqlApiKeyKeyPath,

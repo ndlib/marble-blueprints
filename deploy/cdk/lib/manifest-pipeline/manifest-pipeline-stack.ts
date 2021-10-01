14fbe3ec032efc27ec54dcd26532f261eca4bf85
Jon Hartzler <jon.hartzler@gmail.com>
2021-10-01 12:39:31 -0400

    test type

--- deploy/cdk/lib/manifest-pipeline/manifest-pipeline-stack.ts
+++ deploy/cdk/lib/manifest-pipeline/manifest-pipeline-stack.ts
@@ -250,7 +250,7 @@ export class ManifestPipelineStack extends Stack {
     })
 
     // add back up to the table but only prod
-    console.log(" hi hi hi what is the backup", props.createBackup)
+    console.log(" hi hi hi what is the backup", props.createBackup, typeof props.createBackup)
     if (props.createBackup) {
       console.log("in the backup !! ")
       const plan = backup.BackupPlan.dailyMonthly1YearRetention(this, 'MarbleDynamoDbBackupPlan')

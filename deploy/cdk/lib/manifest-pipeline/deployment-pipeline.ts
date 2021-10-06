e576ffe653ac8dfcab643c36e13c0571ee421aa2
Jon Hartzler <jon.hartzler@gmail.com>
2021-10-06 09:44:35 -0400

    remove test backup

--- deploy/cdk/lib/manifest-pipeline/deployment-pipeline.ts
+++ deploy/cdk/lib/manifest-pipeline/deployment-pipeline.ts
@@ -138,7 +138,6 @@ export class DeploymentPipelineStack extends cdk.Stack {
       // all the user to create a backup
       cdkDeploy.project.addToRolePolicy(new PolicyStatement({
         actions: [
-/*
           'backup:CreateBackupPlan',
           'backup:CreateBackupSelection',
           'backup:CreateBackupVault',
@@ -150,14 +149,17 @@ export class DeploymentPipelineStack extends cdk.Stack {
           'backup:DeleteBackupVaultAccessPolicy',
           'backup:DeleteBackupVaultNotification',
           'backup:DeleteBackupPlan',
-        'backup:DeleteBackupVault',
           'backup:DeleteBackupVault',
-          */
-          'backup:*',
-          'backup-storage:MountCapsule',
+          'backup:DeleteBackupVault',
+          'backup-storage:*',
+          'kms:*'
         ],
         resources: [
-          '*'
+          cdk.Fn.sub('arn:aws:backup:${AWS::Region}:${AWS::AccountId}:backup-plan:*'),
+          cdk.Fn.sub('arn:aws:backup:${AWS::Region}:${AWS::AccountId}:backup-vault:*'),
+          cdk.Fn.sub('arn:aws:backup:${AWS::Region}:${AWS::AccountId}:backup-storage:*'),
+          cdk.Fn.sub('arn:aws:backup:${AWS::Region}:${AWS::AccountId}:key:*'),
+          cdk.Fn.sub('arn:aws:dynamodb:${AWS::Region}:${AWS::AccountId}:table/*/backup/*'),
          ],
       }))
 

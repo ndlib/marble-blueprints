58094585be6d07097f53bdf4b2d65f5ba64f8241
dan <danthewolfe@users.noreply.github.com>
2022-03-31 15:19:08 -0400

    Revert "refactor template to cdk (#355)"
    This reverts commit 7bf352922cfa6b6ff15eacbbec81fa5e42a65ef8.

--- deploy/cdk/lib/iiif-serverless/iiif-serverless-stack.ts
+++ deploy/cdk/lib/iiif-serverless/iiif-serverless-stack.ts
@@ -1,16 +1,17 @@
-import apigateway = require('@aws-cdk/aws-apigateway')
-import { CfnOutput, Construct, Duration, NestedStack, NestedStackProps, Stack, StackProps } from "@aws-cdk/core"
+import { Annotations, CfnInclude, Stack, StackProps, Construct, Duration, Fn, NestedStack, NestedStackProps } from "@aws-cdk/core"
+import { DomainName, BasePathMapping, RestApi } from "@aws-cdk/aws-apigateway"
 import { FoundationStack } from "../foundation"
 import { CnameRecord } from "@aws-cdk/aws-route53"
-import { Function, Runtime } from "@aws-cdk/aws-lambda"
-import { AssetHelpers } from '../asset-helpers'
-import { Effect, PolicyStatement } from '@aws-cdk/aws-iam'
-import * as path from "path"
-
+import YAML = require('yaml')
+import * as fs from "fs"
+import { Asset } from '@aws-cdk/aws-s3-assets'
+// import { CfnInclude } from "@aws-cdk/cloudformation-include"  // the CfnInclude from the aws-cdk/core has been deprecated, but I can't get this replacement to work with the YAML file we need.  https://github.com/aws/aws-cdk/issues/10926
 
 export interface IIiifServerlessStackProps extends StackProps {
   /**
    * The path to the root of the local copy of the serverless-iiif repo.
+   * Assumes all dependencies in this directory have already been built following
+   * https://github.com/nulib/serverless-iiif/blob/master/CONTRIBUTING.md#build-dependencies
    */
   readonly serverlessIiifSrcPath: string
 
@@ -40,127 +41,129 @@ export interface IIiifApiStackProps extends NestedStackProps {
   readonly serverlessIiifSrcPath: string
   readonly foundationStack: FoundationStack
   readonly paramPathPrefix: string
-  readonly hostnamePrefix: string
-  readonly createDns: boolean
 }
 
 /**
- * Creates an Api stack using the template from the source repo
+ * Creates an Api stack using the template from the source repo. It does have to modify the imported
+ * template to work in cdk in a few ways. This ideally will all go away if we decide to just create
+ * the API entirely in this cdk app without using the template from nulib
  */
 class ApiStack extends NestedStack {
   readonly apiName: string
+  readonly apiId: string
 
   constructor(scope: Construct, id: string, props: IIiifApiStackProps) {
     super(scope, id, props)
 
     this.apiName = `${this.stackName}-api`
+    this.apiId = Fn.importValue(`${this.stackName}:ApiId`)
 
-    const iiifFunc = new Function(this, "IiifFunction", {
-      runtime: Runtime.NODEJS_12_X,
-      code: AssetHelpers.codeFromAsset(this, path.join(props.serverlessIiifSrcPath, 'src/')),
-      handler: 'index.handler',
-      timeout: Duration.seconds(10),
-      memorySize: 3072,
-      environment: {
-        tiffBucket: props.foundationStack.publicBucket.bucketName,
-      },
-      initialPolicy: [
-        new PolicyStatement({
-          effect: Effect.ALLOW,
-          actions: [
-            's3:ListBucket',
-            's3:GetBucketLocation',
-          ],
-          resources: [
-            props.foundationStack.publicBucket.bucketArn,
-          ],
-        }),
-        new PolicyStatement({
-          effect: Effect.ALLOW,
-          actions: [
-            's3:GetObject',
-            's3:GetObjectACL',
-          ],
-          resources: [
-            `${props.foundationStack.publicBucket.bucketArn}/*`,
-          ],
-        }),
-        new PolicyStatement({
-          effect: Effect.ALLOW,
-          actions: ['s3:ListAllMyBuckets'],
-          resources: ['*'],
-        }),
-      ],
+    // TODO: At some point we need to migrate this to just deploy from nulib's SAR, or deploy via
+    // our own API definition. When we do, need to enable detailed metrics on this API for per method metrics.
+    // Until then we have to maintain this via the console.
+    if(!fs.existsSync(`${props.serverlessIiifSrcPath}/src`)) {
+      Annotations.of(scope).addError(`Cannot deploy this stack. Asset path not found ${props.serverlessIiifSrcPath}/src`)
+      return
+    }
+    const lambdaAsset = new Asset(this, 'LambdaAsset', {
+      path: `${props.serverlessIiifSrcPath}/src`,
     })
 
-    const fqdn = `${props.hostnamePrefix}.${props.foundationStack.hostedZone.zoneName}`
-    const apiProps = {
-      restApiName: this.apiName,
-      handler: iiifFunc,
-      proxy: false,
-      defaultCorsPreflightOptions: {
-        allowOrigins: [ '*' ],
-        allowMethods: [ 'GET' ],
-      },
-      endpointConfiguration: {
-        types: [ apigateway.EndpointType.REGIONAL ],
-      },
-      domainName: {
-        domainName: fqdn,
-        certificate: props.foundationStack.certificate,
-      },
-      deployOptions: {
-        cacheClusterEnabled: true,
-        cacheClusterSize: '0.5',
-        cacheTtl: Duration.seconds(3600),
-      },
+    if(!fs.existsSync(`${props.serverlessIiifSrcPath}/dependencies`)) {
+      Annotations.of(scope).addError(`Cannot deploy this stack. Asset path not found ${props.serverlessIiifSrcPath}/dependencies`)
+      return
     }
+    const lambdaDepsAsset = new Asset(this, 'SampleSingleFileAsset', {
+      path: `${props.serverlessIiifSrcPath}/dependencies`,
+    })
 
-    const iiifApi = new apigateway.LambdaRestApi(this, 'IiifApi', apiProps)
-    const rootPath = iiifApi.root.addResource('iiif')
-    const twoPath = rootPath.addResource('2')
+    if (!fs.existsSync(`${props.serverlessIiifSrcPath}/template.yml`)) {
+      Annotations.of(scope).addError(`Cannot deploy this stack. Asset path not found ${props.serverlessIiifSrcPath}/template.yml`)
+      return
+    }
+    const iiifTemplate = new CfnInclude(this, "IiifTemplate", {
+      template: YAML.parse(fs.readFileSync(`${props.serverlessIiifSrcPath}/template.yml`).toString()),
+      // templateFile: `${props.serverlessIiifSrcPath}/template.yml`,  // this is the required syntax for "@aws-cdk/cloudformation-include", but causes "Maximum call stack size exceeded" error
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    }) as any
 
-    // /iiif/2/{id}
-    const idPath = twoPath.addResource('{id}')
-    idPath.addMethod('GET')
+    // Can't just ref the public bucket name cause it will create a Fn::ImportValue which will
+    // not work as a param. Instead, the foundation stack will write the bucket name to a param
+    // so we need to change the param to use ssm and set the default
+    iiifTemplate.template.Parameters.SourceBucket.Type = 'AWS::SSM::Parameter::Value<String>'
+    iiifTemplate.template.Parameters.SourceBucket.Default = props.foundationStack.publicBucketParam
 
-    // /iiif2/{id}/info.json
-    const infoPath = idPath.addResource('info.json')
-    infoPath.addMethod('GET')
+    iiifTemplate.template.Parameters.CacheEnabled.Type = 'AWS::SSM::Parameter::Value<String>'
+    iiifTemplate.template.Parameters.CacheEnabled.Default = `${props.paramPathPrefix}/cacheEnabled`
 
-    // /iiif/2/{id}/{proxy+}
-    const idProxyPath = idPath.addProxy({ anyMethod: false })
-    idProxyPath.addMethod('GET')
+    iiifTemplate.template.Parameters.CacheTtl.Type = 'AWS::SSM::Parameter::Value<String>'
+    iiifTemplate.template.Parameters.CacheTtl.Default = `${props.paramPathPrefix}/cacheTtl`
 
-    if(props.createDns) {
+    // There's currently no way to have cdk transform the codeuri's in the same way that it's done when 
+    // using sam or cloudformation cli. Have to find the resources and manually change this
+    iiifTemplate.template.Resources.IiifFunction.Properties.CodeUri = { Bucket: lambdaAsset.s3BucketName, Key: lambdaAsset.s3ObjectKey }
+    iiifTemplate.template.Resources.Dependencies.Properties.ContentUri = { Bucket: lambdaDepsAsset.s3BucketName, Key: lambdaDepsAsset.s3ObjectKey }
+    
+    // Cdk makes the name too long to create the LayerName. Just remove this prop and let cloudformation do it
+    delete iiifTemplate.template.Resources.Dependencies.Properties.LayerName
+  }
+}
+
+export interface IIiifDomainStackProps extends NestedStackProps {
+  readonly hostnamePrefix: string
+  readonly foundationStack: FoundationStack
+  readonly createDns: boolean
+  readonly apiStack: ApiStack
+}
+
+/**
+ * Adds a custom domain to the Api, with or without a dns entry
+ */
+class DomainStack extends NestedStack {
+  constructor(scope: Construct, id: string, props: IIiifDomainStackProps) {
+    super(scope, id, props)
+    const restApi = Fn.importValue(`${props.apiStack.stackName}:ApiId`)
+    const iiifApi = RestApi.fromRestApiId(this, 'IiifApi', restApi)
+    iiifApi.deploymentStage = {
+      restApi,
+      stageName: 'latest',
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any
+
+    const fqdn = `${props.hostnamePrefix}.${props.foundationStack.hostedZone.zoneName}`
+    const domainName = new DomainName(this, 'APIDomain', {
+      domainName: fqdn,
+      certificate: props.foundationStack.certificate,
+    })
+    new BasePathMapping(this, 'RootToLatestStageMapping', {
+      domainName,
+      restApi: iiifApi,
+      stage: iiifApi.deploymentStage,
+    })
+
+    if (props.createDns) {
       new CnameRecord(this, `HostnamePrefix-Route53CnameRecord`, {
         recordName: props.hostnamePrefix,
-        domainName: fqdn,
+        domainName: domainName.domainNameAliasDomainName,
         zone: props.foundationStack.hostedZone,
         ttl: Duration.minutes(15),
       })
     }
-
-    new CfnOutput(this, 'ApiEndpointUrl', {
-      value: `${iiifApi.url}/iiif/2/`,
-      description: 'IIIF Endpoint URL',
-    })
-    new CfnOutput(this, 'ApiId', {
-      value: iiifApi.restApiId,
-      description: 'API Gateway ID',
-    })
   }
 }
-
 
 /**
  * Creates a serverless-iiif stack with a custom domain
  */
 export class IiifServerlessStack extends Stack {
   readonly apiStack: ApiStack
+  readonly domainStack: DomainStack
 
   constructor(scope: Construct, id: string, props: IIiifServerlessStackProps) {
     super(scope, id, props)
+    // Creating this as nested stacks to more easily create dependency between the resources in
+    // the API stack and the Custom Domain
     this.apiStack = new ApiStack(this, 'IiifApiStack', props)
+    this.domainStack = new DomainStack(this, 'IiifDomainStack', { apiStack: this.apiStack, ...props })
   }
 }
